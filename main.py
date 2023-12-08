ea2b0c08c7e068d2530416a4ed1f764099a24347
MiroslavMalisek <100050778+MiroslavMalisek@users.noreply.github.com>
2023-12-08 23:12:37 +0100

    imageNet analysis

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from tensorflow.keras.applications.efficientnet import preprocess_input, decode_
 from keras.models import load_model
 
 
-
 def img_reshape(img):
     height = 110
     width = 110
@@ -59,8 +58,8 @@ def get_predictions_test(model, dataset):
 
 
 batch_size = 32
-img_height = 260
-img_width = 260
+img_height = 180
+img_width = 180
 
 #Zdroj: Seminar10
 base_dir = 'images'
@@ -90,7 +89,7 @@ AUTOTUNE = tf.data.AUTOTUNE
 train_ds_cache = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 
-"""
+
 #----------------plot images + animals count--------------------------------------
 #zdroj: https://kanoki.org/2021/05/11/show-images-in-grid-inside-jupyter-notebook-using-matplotlib-and-numpy/
 img_arr_to_show = []
@@ -123,15 +122,51 @@ for animal_name in class_names:
                                "Total_count": (len(train_animal_images) + len(test_animal_images))})
 
 animals_count_df = pd.DataFrame(animals_count_list, columns=['Animal', 'Train_count', 'Test_count', 'Total_count'])
-"""
+
 
 #------------------------Imagenet analysis-----------------------------------------
-#model = EfficientNetB2(weights='imagenet')
-#model.save('efficientNetB2.keras')
-model = load_model('efficientNetB2.keras')
-#for el, lab in train_ds:
-#    np.squeeze(el, axis=0)
-#    print()
+def get_predictions_imagenet(model, dataset):
+    y_pred = []  # store predicted labels
+    y_true = []  # store true labels
 
-y_true, y_pred_classes, class_labels = get_predictions_train(model, train_ds)
+    # iterate over the dataset
+    for image_batch, label_batch in dataset:
+        image_batch = tf.image.resize(image_batch, (260, 260))
+        y_true.append(label_batch)
+        preds = model.predict(image_batch, verbose=1)
+        decoded_preds = decode_predictions(preds, top=1)
+        predicted_classes = [pred[0][1] for pred in decoded_preds]
+        y_pred.append(predicted_classes)
+
+    class_labels = list(dataset.class_names)
+
+    # convert the true and predicted labels into tensors
+    y_true = tf.concat([item for item in y_true], axis=0)
+    y_pred_classes = tf.concat([item for item in y_pred], axis=0)
+    return y_true, y_pred_classes, class_labels
+
+
+model = EfficientNetB2(weights='imagenet')
+model.save('efficientNetB2.keras')
+# model = load_model('efficientNetB2.keras')
+
+# zdroj: seminar 11
+# get predictions for train and test images (all images)
+y_train_true, y_train_pred_classes, train_class_labels = get_predictions_imagenet(model, train_ds)
+y_test_true, y_test_pred_classes, test_class_labels = get_predictions_imagenet(model, test_ds)
+y_true_np = y_train_true.numpy()
+# concatenate true values for train and test images
+y_true_np = np.append(y_true_np, y_test_true.numpy())
+# concatenate predicted values for train and test images
+y_pred_np = y_train_pred_classes.numpy()
+y_pred_np = np.append(y_pred_np, y_test_pred_classes.numpy())
+# change numbers for string names of classes
+y_true_classes = [train_class_labels[number] for number in y_true_np]
+imagenet_preds_df = pd.DataFrame({'True class': y_true_classes, 'Predicted class': y_pred_np})
+
+# zdroj: chatgpt
+# get top 3 predictions for each class (animal)
+counts = imagenet_preds_df.groupby(['True class', 'Predicted class']).size().reset_index(name='count')
+counts.sort_values(['True class', 'count'], ascending=[True, False], inplace=True)
+top_3_kinds = counts.groupby('True class').head(3)
 print()
